0f7fbe74896459dd17805098866829304fefb423
Swapnil-CS775 <swapnilgavali1414@gmail.com>
2026-01-19 23:16:41 +0530

    solved

---
+++ src/com/volume1/Question15.java
@@ -0,0 +1,9 @@
+package com.volume1;
+
+public class Question15 {
+    public static void main(String[] args) {
+        //implement LL for it
+        // just find sum of all ele in LL
+        // than subtract ele from head and check is it satisfy condition if yes than return LL else continue
+    }
+}
